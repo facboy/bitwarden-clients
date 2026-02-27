0df571d9579c0bad325c45eda4708b831b82daa3
Bernd Schoolmann <mail@quexten.com>
2026-02-27 17:36:03 +0100

    [PM-31406] Introduce dangerous-compat crypto implementations (#19220)
    * Add dangerous compat functions
    
    * Fix eslint

---
+++ libs/common/src/key-management/crypto/dangerous/index.ts
@@ -0,0 +1,5 @@
+// Provides legacy / compatibility crypto that should not be used by anything other than the external
+// integration.
+
+export { DANGEROUS_aesDecryptDuckDuckGoNoPaddingAes256CbcHmac } from "./dangerous_duckduckgo_crypto";
+export { DANGEROUS_aesEcbDecryptLastpassImport } from "./dangerous_lastpass_crypto";
